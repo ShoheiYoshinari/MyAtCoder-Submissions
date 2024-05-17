e3c7412bb12d38266aaf4a6092e59733adc0e83e
AutoCloneBot <acautoclone@bot.com>
2024-05-17 14:49:16 +0000

    Added my codes from AtCoder

---
+++ abc144/abc144_a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(a) (a).begin(), (a).end()
+using Graph = vector<vector<int>>;
+const int dx[]={1,1,1,0,0,-1,-1,-1};
+const int dy[]={1,0,-1,1,-1,1,0,-1};
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, 1, 0, -1};
+const double PI = acos(-1);
+
+int a, b;
+int main(){
+    cin >> a >> b;
+    if(a >= 10 || b >= 10) cout << "-1" << endl;
+    else cout << a*b << endl;
+    return 0;
+}
