eb2a9020020f5789d0677661deba38ec79f9b511
AutoCloneBot <acautoclone@bot.com>
2024-05-29 14:49:01 +0000

    Added my codes from AtCoder

--- tessoku-book/tessoku_book_cq.cpp
+++ tessoku-book/tessoku_book_cq.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i, n) for (ll i = 0; i <= (ll)(n); i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i <= (int)(n); i++)
-
-ll n, s, a[69];
-bool dp[69][10009];
+#define all(a) (a).begin(), (a).end()
+using Graph = vector<vector<int>>;
+const int dx[]={1,1,1,0,0,-1,-1,-1};
+const int dy[]={1,0,-1,1,-1,1,0,-1};
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, 1, 0, -1};
+const double PI = acos(-1);
 
 int main(){
+    int n, s;
+    int a[69];
+    bool dp[69][100009];
     cin >> n >> s;
     rep2(i, 1, n) cin >> a[i];
 
     rep2(i, 0, n){
         rep2(j, 0, s) dp[i][j] = false;
     }
-
     dp[0][0] = true;
+
     rep2(i, 1, n){
         rep2(j, 0, s){
             if(j < a[i]){
@@ -27,29 +34,28 @@ int main(){
             }
         }
     }
-    if(!dp[n][s]){
-        cout << "-1" << endl;
-        return 0;
-    }
 
-    vector<int> ans;
-    int place = n, sum = s;
+    if(dp[n][s]){
+        vector<int> root;
+        int place = n;
+        int value = s;
+        while(true){
+            if(value == 0) break;
 
-    while(true){
-        if(sum == 0) break;
-        if(dp[place-1][sum] == true) place--;
-        else{
-            sum -= a[place];
-            ans.push_back(place);
-            place--;
+            if(dp[place-1][value] == true) place--;
+            else{
+                value -= a[place];
+                root.push_back(place);
+                place--;
+            }
         }
+        reverse(all(root));
+        cout << root.size() << endl;
+        rep(i, root.size()) cout << root[i] << ' ';
+        cout << endl;
+    }else{
+        cout << -1 << endl;
     }
-    reverse(ans.begin(), ans.end());
-    cout << ans.size() << endl;
-    rep2(i, 0, ans.size()-1){
-        cout << ans[i] << ' ';
-    }
-    cout << endl;
     return 0;
-
+    
 }
