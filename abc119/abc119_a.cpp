d3dff09fd31755e70f602098055b44e54b51cc70
AutoCloneBot <acautoclone@bot.com>
2024-12-14 14:49:54 +0000

    Added my codes from AtCoder

---
+++ abc119/abc119_a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+const int dy[] = {1, 0, -1, 0};
+const int dx[] = {0, 1, 0, -1};
+
+struct INIT{
+INIT(){
+std::ios::sync_with_stdio(false);
+std::cin.tie(0);
+cout << fixed << setprecision(20);
+}
+}INIT;
+
+int main(){
+    string s;
+    cin >> s;
+    string year = s.substr(0, 4);
+    string month = s.substr(5, 2);
+    string day = s.substr(8, 2);
+    if(year <= "2019"){
+        if(month <= "04"){
+            if(day <= "30"){
+                cout << "Heisei" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "TBD" << endl;
+    return 0;
+}
---
+++ abc146/abc146_c.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+const int dy[] = {1, 0, -1, 0};
+const int dx[] = {0, 1, 0, -1};
+
+struct INIT{
+INIT(){
+std::ios::sync_with_stdio(false);
+std::cin.tie(0);
+cout << fixed << setprecision(20);
+}
+}INIT;
+
+ll a, b, x;
+
+ll check(ll mid){
+    return a*mid + b*to_string(mid).size();
+}
+
+int main(){
+    cin >> a >> b >> x;
+    ll ok = 0;
+    ll ng = 1e9+1;
+    while(abs(ok - ng) > 1){
+        ll mid = (ok + ng) / 2;
+        if(check(mid) <= x) ok = mid;
+        else ng = mid;
+        // cout << ok << endl;
+    }
+    cout << ok << endl;
+    return 0;
+}
