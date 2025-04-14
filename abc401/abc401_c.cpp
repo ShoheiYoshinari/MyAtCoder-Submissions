f01076c3b7c9d0bd3684077d27b53819b9904ac2
AutoCloneBot <acautoclone@bot.com>
2025-04-14 14:51:08 +0000

    Added my codes from AtCoder

--- abc401/abc401_c.cpp
+++ abc401/abc401_c.cpp
@@ -1,97 +1,50 @@
 #include <bits/stdc++.h>
+#include <cstdio>
 using namespace std;
-using ll = long long;
-using ull = unsigned long long;
-const double pi = acos(-1);
-#define OVERLOAD_REP(_1, _2, _3, name, ...) name
-#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
-#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
-#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
-#define all(p) (p).begin(), (p).end()
-#define exists(c, e) ((c).find(e) != (c).end())
-template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
-template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
-const int dy[] = {1, 0, -1, 0};
-const int dx[] = {0, 1, 0, -1};
 
-struct INIT{
- INIT(){
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
-  cout << fixed << setprecision(20);
- }
-}INIT;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define reps(i,n) for (int i=1; i<=(n); i++)
+#define for_(i,a,b) for (int i=(a); i<(b); i++)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
 
-template <std::uint_fast64_t Modulus> class modint {
-	using u64 = std::uint_fast64_t;
-public:
-	u64 a;
-	constexpr modint(const u64 x = 0) noexcept : a(x % Modulus) {}
-	constexpr u64 &value() noexcept { return a; }
-	constexpr const u64 &value() const noexcept { return a; }
-	constexpr modint operator+(const modint rhs) const noexcept {
-		return modint(*this) += rhs;
-	}
-	constexpr modint operator-(const modint rhs) const noexcept {
-		return modint(*this) -= rhs;
-	}
-	constexpr modint operator*(const modint rhs) const noexcept {
-    	return modint(*this) *= rhs;
-	}
-	constexpr modint operator/(const modint rhs) const noexcept {
-		return modint(*this) /= rhs;
-	}
-	constexpr modint &operator+=(const modint rhs) noexcept {
-		a += rhs.a;
-		if (a >= Modulus) {
-			a -= Modulus;
-		}
-    	return *this;
-	}
-	constexpr modint &operator-=(const modint rhs) noexcept {
-		if (a < rhs.a) {
-			a += Modulus;
-		}
-		a -= rhs.a;
-		return *this;
-	}
-	constexpr modint &operator*=(const modint rhs) noexcept {
-    	a = a * rhs.a % Modulus;
-    	return *this;
-	}
-	constexpr modint &operator/=(modint rhs) noexcept {
-		u64 exp = Modulus - 2;
-		while (exp) {
-			if (exp % 2) {
-				*this *= rhs;
-			}
-			rhs *= rhs;
-			exp /= 2;
-		}
-    	return *this;
-	}
-
-    friend std::ostream& operator<<(std::ostream& os, const modint& rhs) {
-        os << rhs.a;
-        return os;
-    }
-};
-using mint = modint<1000000000>;
-
-int main(){
-    ll n, k;
+int main() {
+    
+    long long n, k;
     cin >> n >> k;
-    vector<mint> a(n+1), s(n+1);
+    long long m = 0;
+
+    vector<long long> cs(n+1, 0);
 
     rep(i, n+1){
         if(i < k){
-            a[i] = 1;
-            s[i] = (i == 0 ? a[0] : s[i-1] + a[i]);
-        }else{
-            a[i] = (i == k ? s[i-1] : s[i-1] - s[i-k-1]);
-            s[i] = s[i-1] + a[i];
+            if(i == 0){
+                m = 1;
+                cs.at(i) = 1;
+            }else{
+                m = 1;
+                cs.at(i) = abs(cs.at(i-1) + m); 
+            }
+            
+        }else if (i == k){
+            m = cs.at(i-1);
+            cs.at(i) = abs(cs.at(i-1) + m);
+        }else if(i > k){
+			long long t = cs.at(i-1) - cs.at(i-(k+1));
+			if(t < 0) m = t + 1000000000;
+            else m = t;
+            cs.at(i) = abs(cs.at(i-1) + m);
         }
+
+        m %= 1000000000;
+        cs.at(i) %= 1000000000;
+        
     }
-    cout << a[n] << endl;
+
+    long long ans = m % 1000000000;
+    cout << ans << endl;
+
     return 0;
 }
