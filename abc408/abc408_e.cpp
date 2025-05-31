3e6edeafd7f606b1a250e9048298b48677302915
AutoCloneBot <acautoclone@bot.com>
2025-05-31 14:48:58 +0000

    Added my codes from AtCoder

---
+++ abc408/abc408_e.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, 1, 0, -1};
+
+struct INIT{
+INIT(){
+std::ios::sync_with_stdio(false);
+std::cin.tie(0);
+cout << fixed << setprecision(20);
+}
+}INIT;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<pair<int, ll>>> g(n);
+    rep(i, m){
+        int u, v, w;
+        cin >> u >> v >> w;
+        u--, v--;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
+    }
+    
+    ll ans = 0;
+    ll mask = 0;
+    for (int bit = 30; bit >= 0; bit--) {
+        vector<bool> visited(n, false);
+        queue<int> q;
+        q.push(0);
+        visited[0] = true;
+        while (!q.empty()) {
+            auto v = q.front(); q.pop();
+            for(auto [to, w] : g[v]) {
+                if(w & mask) continue;
+                if(w & (1LL << bit)) continue;
+                if (!visited[to]) {
+                    visited[to] = true;
+                    q.push(to);
+                }
+            }
+        }
+        if(!visited[n-1]) ans += (1LL << bit);
+        else mask += (1LL << bit);
+    }
+
+    cout << ans << endl;
+    return 0;
+}
