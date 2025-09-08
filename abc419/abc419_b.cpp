6f6f74c6500ddf6aa71fcbe342d738e729f4dc27
AutoCloneBot <acautoclone@bot.com>
2025-09-08 14:51:12 +0000

    Added my codes from AtCoder

---
+++ abc419/abc419_b.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, 1, 0, -1};
+
+struct INIT{
+INIT(){
+std::ios::sync_with_stdio(false);
+std::cin.tie(0);
+cout << fixed << setprecision(20);
+}
+}INIT;
+
+int main(){
+    int q;
+    cin >> q;
+
+    vector<int> v;
+    while(q--){
+        int t;
+        cin >> t;
+        if(t == 1){
+            int x;
+            cin >> x;
+            v.push_back(x);
+        }else if(t == 2){
+            sort(all(v));
+            reverse(all(v));
+            // rep(i, v.size()) cout << v[i] << ' ';
+            // cout << endl;
+            cout << v[v.size()-1] << endl;
+            v.pop_back();
+
+        }
+    }
+    return 0;
+}
