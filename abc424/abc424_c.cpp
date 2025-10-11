a9a44cb3b7cd477cba66a6112ff80d9facd257ae
AutoCloneBot <acautoclone@bot.com>
2025-10-11 14:49:12 +0000

    Added my codes from AtCoder

---
+++ abc424/abc424_c.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+const double pi = acos(-1);
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(p) (p).begin(), (p).end()
+#define exists(c, e) ((c).find(e) != (c).end())
+template<class T> bool chmin(T& a,T b) { if(a > b){a = b; return true;} return false; }
+template<class T> bool chmax(T& a,T b) { if(a < b){a = b; return true;} return false; }
+template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, 1, 0, -1};
+
+struct INIT{
+INIT(){
+std::ios::sync_with_stdio(false);
+std::cin.tie(0);
+cout << fixed << setprecision(20);
+}
+}INIT;
+
+using Graph = vector<vector<int>>;
+vector<bool> seen;
+void dfs(const Graph &G, int v) {
+    seen[v] = true;
+    for (auto next_v : G[v]) {
+        if (seen[next_v]) continue;
+        dfs(G, next_v);
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    Graph G(n+1);
+    vector<int> got;
+    rep(i, 1, n+1){
+        int a, b;
+        cin >> a >> b;
+        if(a == 0 && b == 0){
+            got.push_back(i);
+        }else{
+            G[a].push_back(i);
+            G[b].push_back(i);
+        }
+    }
+
+    seen.resize(n+1, false);
+    for(int v : got){
+        seen[v] = true;
+        dfs(G, v);
+    }
+
+    int ans = 0;
+    rep(i, 1, n+1) if(seen[i]) ans++;
+    cout << ans << endl;
+    return 0;
+}
